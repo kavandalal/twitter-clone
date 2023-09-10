65588fb603be437cd98efa2e9bc8940ca47221f2
kavandalal <kavandalal003@gmail.com>
2023-09-10 10:44:41 +0530

    removed unnecessary imports in profile

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,5 +1,5 @@
-import { Prisma } from "@prisma/client";
-import { inferAsyncReturnType } from "@trpc/server";
+// import { Prisma } from "@prisma/client";
+// import { inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 
 import {
